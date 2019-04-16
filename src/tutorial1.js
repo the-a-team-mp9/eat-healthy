ca96555ad02eafeab56442b8ba9a8b3171120a4a
abhiash-sridhara <abhilash.sridhara@gmail.com>
2019-04-16 22:35:42 +1000

    added link to jump to game from tutorial

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -7,7 +7,7 @@ class Tutorial extends React.Component{
 
     render(){
         return(
-            <div className='hero' style={{padding:'20px 0 20px', height:'100vh'}}>
+            <div className='hero' style={{padding:'20px 0 20px', height:'100vh', textAlign:'center'}}>
                 <div className='embed-container' style={{margin:'7%'}}>
                     <iframe src='https://www.youtube.com/embed//NCJ3XOCGbr0' frameBorder='0' 
                     allow="accelerometer; autoplay; encrypted-media; gyroscope;" allowFullScreen="allowfullscreen"
@@ -16,6 +16,9 @@ class Tutorial extends React.Component{
                     oallowfullscreen="oallowfullscreen" 
                     webkitallowfullscreen="webkitallowfullscreen" />
                 </div>
+                <a className='learn-more-btn' href='/game-1'>
+                    Jump to Game
+                </a>
                 {/* <iframe width="auto" height="auto" src="https://www.youtube.com/embed/NCJ3XOCGbr0" frameborder="0" 
                     allow="accelerometer; autoplay; encrypted-media; gyroscope;" allowfullscreen="allowfullscreen"
                     mozallowfullscreen="mozallowfullscreen" 
