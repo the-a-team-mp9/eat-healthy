744f5cf09ced0be06709265ee6e8ae22b366803c
abhiash-sridhara <abhilash.sridhara@gmail.com>
2019-04-20 22:57:13 +1000

    game 2 dice timining adjust

--- src/game-2-controller.js
+++ src/game-2-controller.js
@@ -28,7 +28,7 @@ class Game2 extends React.Component
             document.getElementById('dice-gif').style.display='none';
             document.getElementById(d).style.display='block';
 
-        },1600,choice);
+        },1200,choice);
     }
 
     render(){
