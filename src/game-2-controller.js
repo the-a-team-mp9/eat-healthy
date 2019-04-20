dc9bab0c5c664ea92ec53db3a5a9139f52fadb14
abhiash-sridhara <abhilash.sridhara@gmail.com>
2019-04-20 22:49:36 +1000

    added dice roll functionality to game2

--- src/game-2-controller.js
+++ src/game-2-controller.js
@@ -3,6 +3,34 @@ import {ProgressBar} from 'react-bootstrap'
 
 class Game2 extends React.Component
 {
+    constructor(props){
+        super(props);
+    }
+
+    roll1(){
+        let rnd = Math.random();
+        let choice;
+        if (rnd<0.33)
+            choice='dice1';
+        else if(rnd>=0.33 && rnd <=0.66)
+            choice='dice2';
+        else
+            choice='dice3';
+        let d1=document.getElementById('dice1');
+        let d2=document.getElementById('dice2');
+        let d3=document.getElementById('dice3');
+        d1.style.display='none';
+        d2.style.display='none';
+        d3.style.display='none';
+        document.getElementById('dice-gif').style.display='block';
+        // console.log(document.getElementById('dice-gif').style.display);
+        setTimeout((d)=>{            
+            document.getElementById('dice-gif').style.display='none';
+            document.getElementById(d).style.display='block';
+
+        },2000,choice);
+    }
+
     render(){
         return(
             <div className='game-2-area'>
@@ -28,8 +56,13 @@ class Game2 extends React.Component
                 </div>
                 <div id='p2char'> 
                 </div>
-                <div id='dice'>
+                <div id='dice1' onClick={this.roll1}>
                 </div>
+                <div id='dice2' onClick={this.roll1}>
+                </div>
+                <div id='dice3' onClick={this.roll1}>                
+                </div>
+                <div id='dice-gif'></div>
                 <div id='yes'>
                 </div>
                 <div id='no'>
