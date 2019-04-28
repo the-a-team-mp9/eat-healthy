1bb2a61f0abee5a84283fd3c51b85acdb79d1b1a
abhiash-sridhara <abhilash.sridhara@gmail.com>
2019-04-28 10:49:28 +1000

    env config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+console.log(process.env.API_KEY);
 
 const config={
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID.toString(),
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MSG_SENDER_ID
 }
